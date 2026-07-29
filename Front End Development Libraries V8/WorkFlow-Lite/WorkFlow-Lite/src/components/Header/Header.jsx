6d3515bb01c961461c9b5aefd447b5f2a5ed8657
onpux <onputrevor1@gmail.com>
2026-07-29 15:42:39 -0500

    Delete Front End Development Libraries V8/WorkFlow-Lite directory

--- Front End Development Libraries V8/WorkFlow-Lite/WorkFlow-Lite/src/components/Header/Header.jsx
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-  return (
-    <header className="d-flex justify-content-between align-items-center py-3 border-bottom">
-      <h1 className="h4 mb-0">WorkFlow Lite</h1>
-    </header>
-  );
-}
--- Front End Development Libraries V8/WorkFlow-Lite/src/app/hooks.js
+++
@@ -1,6 +0,0 @@
-import { useDispatch, useSelector } from 'react-redux';
-
-// Punto único de importación de los hooks de Redux.
-// Si en el futuro se migra a TypeScript, solo este archivo cambia.
-export const useAppDispatch = () => useDispatch();
-export const useAppSelector = useSelector;
