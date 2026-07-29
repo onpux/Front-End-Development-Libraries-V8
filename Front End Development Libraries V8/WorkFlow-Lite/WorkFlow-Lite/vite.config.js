f783f9a1c3d9a2e0a03af83c369f5a31610fda5b
onpux <onputrevor1@gmail.com>
2026-07-29 15:37:22 -0500

    Add files via upload

--- Front End Development Libraries V8/WorkFlow-Lite/WorkFlow-Lite/vite.config.js
+++ Front End Development Libraries V8/WorkFlow-Lite/WorkFlow-Lite/vite.config.js
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Si tu repo se llama distinto a "WorkFlow-Lite", cambia esta línea.
-  // Si es <usuario>.github.io (sitio raíz), bórrala o déjala como ''.
-  base: '/WorkFlow-Lite/',
+  // Subpath de GitHub Pages: https://onpux.github.io/Front-End-Development-Libraries-V8/
+  base: '/Front-End-Development-Libraries-V8/',
   plugins: [react()],
   test: {
     globals: true,
